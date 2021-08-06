4bb0f00a7297e409e11803cc365c00d2f79ae0aa
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-08-06 12:57:21 -0400

    Created separate .js file for prompt questions. Fixed inquirer installation

--- Develop/index.js
+++ Develop/index.js
@@ -1,39 +1,10 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
+const questions = require('./utils/questions');
 
 inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?',
-    },
-    {
-      type: 'input',
-      message: 'Where are you from?',
-      name: 'location',
-    },
-    {
-      type: 'input',
-      message: 'What is your favorite hobby?',
-      name: 'hobby',
-    },
-    {
-        type: 'input',
-        message: 'What is your LinkedIn username?',
-        name: 'linkedIn',
-    },
-    {
-        type: 'input',
-        message: 'What is your GitHub username?',
-        name: 'linkedin',
-    },
-  ])
+  .prompt(questions)
   .then((data) => {
-    const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
-
-    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
-      err ? console.log(err) : console.log('Success!')
-    );
+    console.log(data);
   });
---
+++ Develop/utils/questions.js
@@ -0,0 +1,28 @@
+const questions = [{
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?',
+  },
+  {
+    type: 'input',
+    message: 'Where are you from?',
+    name: 'location',
+  },
+  {
+    type: 'input',
+    message: 'What is your favorite hobby?',
+    name: 'hobby',
+  },
+  {
+      type: 'input',
+      message: 'What is your LinkedIn username?',
+      name: 'linkedIn',
+  },
+  {
+      type: 'input',
+      message: 'What is your GitHub username?',
+      name: 'linkedin',
+  },
+]
+
+module.exports = questions;
