4cc42dc9a0df80ca52bd48f1cb8a9a34c5d53800
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-08-17 14:33:49 -0400

    Created basic HTML file that uses user input

--- Develop/utils/createHtml.js
+++ Develop/utils/createHtml.js
@@ -7,7 +7,24 @@ function writeToFile(fileName, data){
 }
 
 function createHtml(data){
-    return 'Success!';
+    return `<!DOCTYPE html>
+    <html lang="en-US">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Create Your Own HTML!</title>
+            <!-- <link rel="stylesheet" href="./assets/css/reset.css"> -->
+            <link rel="stylesheet" href="./assets/css/style.css">
+        </head>
+        <body>
+            <h1>Hello! My name is ${data.name}</h1>
+            <p>I am from ${data.location}</p>
+            <p>My favorite hobby is ${data.hobby}</p>
+            <p><b>GitHub:</b> <a href='https://github.com/${data.github} target='_blank'>${data.github}</a></p>
+            <p><b>LinkedIn:</b> <a href='https://linkedin.com/in/${data.linkedIn}' target = '_blank'>${data.linkedIn}</a></p>
+        </body>
+    </html>`;
 }
 
 module.exports = { createHtml, writeToFile };
